43a227182fde3f2f107009c7d666058eff4f2c14
Christiano Haesbaert <haesbaert@haesbaert.org>
2022-07-07 02:25:37 +0200

    Cut the fat now that we're close to release
    Shrinkes the source file by ~50%

--- lib/cpu_stubs.c
+++ lib/cpu_stubs.c
@@ -14,130 +14,33 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#if !defined(__linux__) && !defined(__APPLE__) && \
-    !defined(__FreeBSD__) && !defined(__OpenBSD__) && \
-    !defined(__NetBSD__) && !defined(__DragonFly__)
-#error Unsupported OS
+#ifdef __linux__
+#define _GNU_SOURCE
+#include <sys/sysinfo.h>
 #endif
 
-/*
- * ""Portable"" num_cpu()
- */
-int	num_cpu(void);
-int	num_cpu_online(void);
-
-/*
- *    Linux
- */
-#ifdef __linux__
-
-#define _GNU_SOURCE
-
-#include <sys/sysinfo.h>
-#include <errno.h>
-#include <pthread.h>
-#include <sched.h>
-
-#define USE_NUM_CPU_SYSCONF
-#define USE_AFFINITY_LINUX
-
-#undef _GNU_SOURCE
-#endif	/* __linux__ */
-
-/*
- *    Darwin - __APPLE__
- */
-#ifdef __APPLE__
-
 #include <sys/types.h>
+
+#if defined(__APPLE__) || defined(__FreeBSD__) ||  \
+    defined(__OpenBSD__) || defined(__NetBSD__) || \
+    defined(__NetBSD__) || defined(__DragonFly__)
 #include <sys/sysctl.h>
+#endif
 
-#define USE_NUM_CPU_SYSCONF
-#define USE_NOP_AFFINITY
-#define USE_SYSCTLBYNAME_32
-#define USE_NUM_CORE_APPLE
-#define USE_NUM_SOCKET_APPLE
-
-#endif	/* endif __APPLE__ */
-
-/*
- *    FreeBSD
- */
 #ifdef __FreeBSD__
-
-#include <errno.h>
-#include <pthread.h>
-#include <pthread_np.h>		/* Has CPU_ macros */
-
 #include <sys/cpuset.h>
-
 typedef cpuset_t cpu_set_t;
-
-#define USE_NUM_CPU_SYSCONF
-#define USE_AFFINITY_LINUX	/* Nice enough to have compat */
-
-#endif	/* endif __FreeBSD__ */
-
-/*
- *    OpenBSD
- */
-#ifdef __OpenBSD__
-
-#define USE_NUM_CPU_SYSCONF
-#define USE_NOP_AFFINITY
-
-#endif	/* endif __OpenBSD__ */
-
-/*
- *    NetBSD
- */
-#ifdef __NetBSD__
+#endif
 
 #include <errno.h>
 #include <pthread.h>
 #include <sched.h>
+#include <unistd.h>
 
-#define USE_NUM_CPU_SYSCONF
-#define USE_AFFINITY_LINUX
+#ifdef __FreeBSD__
+#include <pthread_np.h>		/* Has CPU_ macros */
+#endif
 
-/* Mini compat layer for linux affinity */
-typedef cpu_set_t cpuset_t;
-#define CPU_ALLOC(_n)		cpuset_create()
-#define	CPU_FREE(cs)		cpuset_destroy(c)
-#define	CPU_ALLOC_SIZE(c)	cpuset_size(c)
-#define	CPU_ZERO_S(_s, c)	cpuset_zero(c)
-#define	CPU_ISSET_S(i, _s, c)	cpuset_isset(i, c)
-#define	CPU_SET_S(i, _s, c)	cpuset_set(i, c)
-#define	CPU_CLR_S(i, _s, c)	cpuset_clear(i, c)
-
-#endif	/* endif __NetBSD__ */
-
-/*
- *    DragonFly
- */
-#ifdef __DrafonFly__
-
-#include <pthread.h>
-#include <pthread_np.h>
-
-#include <sys/cpumask.h>
-
-#define USE_NUM_CPU_SYSCONF
-#define USE_AFFINITY_LINUX
-
-#define CPU_ALLOC(_n)		malloc(CPU_SETSIZE)
-#define	CPU_FREE(cs)		free(cs)
-#define	CPU_ALLOC_SIZE(_c)	CPU_SETSIZE
-#define	CPU_ZERO_S(_s, c)	CPU_ZERO(c)
-#define	CPU_ISSET_S(i, _s, c)	CPU_ISSET(i, c)
-#define	CPU_SET_S(i, _s, c)	CPU_SET(i, c)
-#define	CPU_CLR_S(i, _s, c)	CPU_CLR(i, c)
-
-#endif	/* endif __DragonFly__ */
-
-/*
- *    Ocaml includes
- */
 #include "caml/memory.h"
 #include "caml/fail.h"
 #include "caml/unixsupport.h"
@@ -146,90 +49,20 @@ typedef cpu_set_t cpuset_t;
 #include "caml/custom.h"
 #include "caml/bigarray.h"
 
-/*
- *    num_cpu() and num_cpu_online()
- */
-#if defined(USE_NUM_CPU_SYSCONF)
+#if defined(__linux__) || defined(__FreeBSD__) /* Nice enough to have compat */
+#define USE_AFFINITY_LINUX
 
-#include <unistd.h>
-
-int
-num_cpu(void)
-{
-	return (int)sysconf(_SC_NPROCESSORS_CONF);
-}
-
-int
-num_cpu_online(void)
-{
-	return (int)sysconf(_SC_NPROCESSORS_ONLN);
-}
-
-#elif defined(USE_NUM_CPU_NPROCS)
-
-int
-num_cpu(void)
-{
-	return (get_nprocs_conf());
-}
-
-int
-num_cpu_online(void)
-{
-	return (get_nprocs());
-}
-
-#elif defined(USE_NUM_CPU_BSD)
-
-int
-num_cpu_bsd(int hw)
-{
-	int mib[] = { CTL_HW, hw };
-	int numcpu;
-	size_t size = sizeof(numcpu);
-
-	if (sysctl(mib, sizeof(mib) / sizeof(mib[0]),
-	    &numcpu, &size, NULL, 0) == -1)
-		return (-1);
-
-	return (numcpu);
-}
+#elif defined(__APPLE__)
+#define USE_NOP_AFFINITY
+#define USE_SYSCTLBYNAME_32
+#define USE_NUM_APPLE
 
 #else
-#error Dont know which num_cpu to use :-(
-#endif	/* USE_NUM_CPU_* */
+#define USE_NOP_AFFINITY
+#endif	/* USE_* */
 
-#ifdef USE_NUM_CORE_APPLE
-
-int
-num_core(void)
-{
-	int32_t core;
-	size_t len = sizeof(core);
-
-	if (sysctlbyname("hw.physicalcpu", &core, &len, NULL, 0) == -1)
-		return (-1);
-
-	return ((int)core);
-}
-
-#endif
-
-#ifdef USE_NUM_SOCKET_APPLE
-
-int
-num_socket(void)
-{
-	int32_t socket;
-	size_t len = sizeof(socket);
-
-	if (sysctlbyname("hw.packages", &socket, &len, NULL, 0) == -1)
-		return (-1);
-
-	return ((int)socket);
-}
-
-#endif
+#define num_cpu()		((int)sysconf(_SC_NPROCESSORS_CONF))
+#define num_cpu_online()	((int)sysconf(_SC_NPROCESSORS_ONLN))
 
 /*
  * Ocaml FFI
@@ -238,26 +71,16 @@ CAMLprim value
 caml_num_cpu(value vunit)
 {
 	CAMLparam0();
-	int n;
 
-	caml_enter_blocking_section();
-	n = num_cpu();
-	caml_leave_blocking_section();
-
-	CAMLreturn (Val_int(n));
+	CAMLreturn (Val_int(num_cpu()));
 }
 
 CAMLprim value
 caml_num_cpu_online(value vunit)
 {
 	CAMLparam0();
-	int n;
 
-	caml_enter_blocking_section();
-	n = num_cpu_online();
-	caml_leave_blocking_section();
-
-	CAMLreturn (Val_int(n));
+	CAMLreturn (Val_int(num_cpu_online()));
 }
 
 #ifdef USE_SYSCTLBYNAME_32
@@ -277,38 +100,6 @@ caml_sysctlbyname32(value mib)
 
 #endif
 
-#ifdef USE_NUM_CORE_APPLE
-
-CAMLprim value /* NOTE remove if we don't use it from C */
-caml_num_core(value vunit)
-{
-	CAMLparam0();
-	int n;
-
-	if ((n = num_core()) == -1)
-		uerror("num_core", Nothing);
-
-	CAMLreturn (Val_int(n));
-}
-
-#endif
-
-#ifdef USE_NUM_SOCKET_APPLE
-
-CAMLprim value /* NOTE remove if we don't use it from C */
-caml_num_socket(value vunit)
-{
-	CAMLparam0();
-	int n;
-
-	if ((n = num_socket()) == -1)
-		uerror("num_socket", Nothing);
-
-	CAMLreturn (Val_int(n));
-}
-
-#endif
-
 /*
  *    set_affinity() and get_affinity()
  */
@@ -320,12 +111,8 @@ caml_set_affinity(value cpulist)
 	CAMLparam1(cpulist);
 	CAMLlocal1(cpu);
 	cpu_set_t cpuset;
-	int numcpus, error;
+	int error;
 	long cpuid;
-
-	caml_enter_blocking_section();
-	numcpus = num_cpu();
-	caml_leave_blocking_section();
 
 	CPU_ZERO(&cpuset);
 
@@ -347,13 +134,9 @@ CAMLprim value
 caml_get_affinity(value unit)
 {
 	cpu_set_t cpuset;
-	int numcpus, error, cpuid;
+	int error, cpuid;
 	CAMLparam0();
 	CAMLlocal2(cpulist, cpu);
-
-	caml_enter_blocking_section();
-	numcpus = num_cpu();
-	caml_leave_blocking_section();
 
 	CPU_ZERO(&cpuset);
 
@@ -364,7 +147,7 @@ caml_get_affinity(value unit)
 	}
 
 	cpulist = Val_emptylist;
-	for (cpuid = numcpus - 1; cpuid >= 0; cpuid--) {
+	for (cpuid = num_cpu() - 1; cpuid >= 0; cpuid--) {
 		if (!CPU_ISSET(cpuid, &cpuset))
 			continue;
 		cpu = caml_alloc(2, Tag_cons);
@@ -391,14 +174,11 @@ caml_get_affinity(value unit)
 {
 	CAMLparam0();
 	CAMLlocal2(cpulist, cpu);
-	int cpuid, numcpus;
+	int cpuid;
 
-	caml_enter_blocking_section();
-	numcpus = num_cpu();
-	caml_leave_blocking_section();
 	/* Assume affinity is all, since we can't set or get */
 	cpulist = Val_emptylist;
-	for (cpuid = numcpus - 1; cpuid >= 0; cpuid--) {
+	for (cpuid = num_cpu() - 1; cpuid >= 0; cpuid--) {
 		cpu = caml_alloc(2, Tag_cons);
 		Store_field(cpu, 0, Val_int(cpuid));
 		Store_field(cpu, 1, cpulist);
