f5c4fb9c339dd232aa65182e9acef5fd70b411a7
Christiano Haesbaert <haesbaert@haesbaert.org>
2022-07-08 16:19:15 +0200

    Missing dots

--- lib/topology.mli
+++ lib/topology.mli
@@ -13,7 +13,7 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
-(** CPU Topology
+(** CPU Topology.
 
     The topology is static and built uppon Module load.
 
